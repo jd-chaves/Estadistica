1a9e3e3eb2fffda2e7cc6d29ca3a29b67150497f
jd-chaves <jd.chaves@uniandes.edu.co>
2019-03-01 12:21:17 -0500

    correccion

--- estadistica.R
+++ estadistica.R
@@ -191,7 +191,6 @@
 
 	make.dir <- function(fp) {
 		if(!file.exists(fp)) {  # If the folder does not exist, create a new one
-		make.dir(dirname(fp))
 		dir.create(fp)
 		} else {   # If it existed, delete and replace with a new one  
 		unlink(fp, recursive = TRUE)
